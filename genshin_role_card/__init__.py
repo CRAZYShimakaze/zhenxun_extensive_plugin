6b38e1d9013da8b9814ad18f55aec17eb346f37e
CRAZYShimakaze <674015283@qq.com>
2022-06-18 11:01:20 +0800

    支持查询指定角色

--- genshin_role_card/__init__.py
+++ genshin_role_card/__init__.py
@@ -16,9 +16,10 @@ usage：
     查询橱窗第一个角色的面板
     指令：
         原神角色卡 uid
+        原神角色卡 uid 角色名
 """.strip()
 __plugin_des__ = "查询橱窗第一个角色的面板"
-__plugin_cmd__ = ["原神角色卡 [uid]"]
+__plugin_cmd__ = ["原神角色卡 [uid] ?[角色名]"]
 __plugin_type__ = ("原神相关", )
 __plugin_version__ = 0.1
 __plugin_author__ = "CRAZYSHIMAKAZE"
@@ -36,27 +37,63 @@ char_card = on_command("原神角色卡", priority=15, block=True)
 
 CARD_PATH = IMAGE_PATH / "genshin" / "char_card"
 CARD_PATH.mkdir(parents=True, exist_ok=True)
+characters = {
+    '钟离': 'zhongli',
+    '神里绫华': 'ayaka',
+    '胡桃': 'hutao',
+    '甘雨': 'ganyu',
+    '雷电将军': 'shougun',
+    '温迪': 'venti',
+    '达达利亚': 'tartaglia',
+    '夜兰': 'yelan',
+    '七七': 'qiqi',
+    '优菈': 'eula',
+    '八重神子': 'yaemiko',
+    '可莉': 'klee',
+    '宵宫': 'yoimiya',
+    '枫原万叶': 'kazuha',
+    '珊瑚宫心海': 'kokomi',
+    '琴': 'jean',
+    '申鹤': 'shenhe',
+    '荒泷一斗': 'itto',
+    '莫娜': 'mona',
+    '迪卢克': 'diluc',
+    '阿贝多': 'albedo',
+    '魈': 'xiao',
+    '神里绫人': 'ayato',
+    '刻晴': 'keqing'
+}
 
 
 @char_card.handle()
 async def _(event: MessageEvent, arg: Message = CommandArg()):
-    msg = arg.extract_plain_text().strip()
+    msg = arg.extract_plain_text().strip().split()
+    #print(msg)
     try:
-        msg = int(msg)
+        uid = int(msg[0])
     except:
         await char_card.send("请输入正确uid...")
         return
-    await char_card.send("开始获取角色信息,预计需要30秒...")
+    if len(msg) == 2:
+        if msg[1] in characters:
+            chara = characters[msg[1]]
+            await char_card.send("开始获取角色信息,预计需要30秒...")
+        else:
+            await char_card.send("请输入正确五星角色名...")
+            return
+    else:
+        chara = 'none'
+        await char_card.send("未指定角色,默认橱窗第一位,预计需要30秒...")
     try:
-        alc_img = await get_alc_image(CARD_PATH, str(msg))
+        alc_img = await get_alc_image(CARD_PATH, str(uid), chara)
     except Exception as e:
         logger.info(f"{e}")
         return
     if alc_img:
-        mes = alc_img + f"\nUID({str(msg)})的角色卡片"
-        await char_card.send(mes)
+        mes = alc_img + f"\nUID({str(uid)})的角色卡片"
+        await char_card.send(mes,at_sender=True)
         logger.info(
             f"(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else 'private'})"
             f" 发送原神角色卡")
     else:
-        await char_card.send(f"获取UID({str(msg)})角色信息失败,请检查是否已开放橱窗信息权限!")
+        await char_card.send(f"获取UID({str(uid)})角色信息失败,请检查是否已开放橱窗详细信息权限或稍后再试!")
--- genshin_role_card/data_source.py
+++ genshin_role_card/data_source.py
@@ -7,23 +7,63 @@ from utils.http_utils import get_browser
 from nonebot.adapters.onebot.v11 import MessageSegment
 from typing import Optional
 import os
+import time
+import pypinyin
+from bs4 import BeautifulSoup
 
 
-async def get_alc_image(path: Path, uid: str) -> Optional[MessageSegment]:
+async def get_alc_image(path: Path, uid: str,
+                        chara: str) -> Optional[MessageSegment]:
     """
     截取卡片
     :param path: 存储路径
     """
-    url = "https://enka.shinshin.moe/u/" + uid
+    #print(chara)
+    url = f"https://enka.shinshin.moe/u/{uid}"
     browser = await get_browser()
     page = await browser.new_page()
-    await page.goto(url, wait_until="networkidle", timeout=100000)
-    await page.set_viewport_size({"width": 2560, "height": 1080})
-    await page.evaluate(
-        "document.getElementsByClassName('Dropdown-list')[0].children[13].dispatchEvent(new Event('click'));"
-    )
-    await page.locator('div.Card').screenshot(path=path / f"{uid}.png")
-    await page.close()
-    #await browser.close()
-    return image(path / f"{uid}.png")
-    #return await AsyncPlaywright.screenshot(url, path / f"{uid}.png", ".Card.svelte-m3ch8z")
+    try:
+        await page.goto(url, wait_until="networkidle", timeout=100000)
+        await page.set_viewport_size({"width": 2560, "height": 1080})
+        await page.evaluate(
+            "document.getElementsByClassName('Dropdown-list')[0].children[13].dispatchEvent(new Event('click'));"
+        )
+        if chara == 'none':
+            await page.locator('div.Card').screenshot(path=path / f"{uid}.png")
+            await page.close()
+            #await browser.close()
+            return image(path / f"{uid}.png")
+        html = await page.inner_html(".CharacterList")
+        soup = BeautifulSoup(html, "html.parser")
+        styles = [figure["style"] for figure in soup.find_all("figure")]
+        #print(styles)
+        index = -1
+        chara_src = ""
+        for i, style in enumerate(styles):
+            if chara in style.lower():
+                index = i
+                chara_src = style
+                break
+        if index == -1 or not chara_src:
+            return
+        await page.locator(f'div.avatar.svelte-188i0pk >> nth={index}').click()
+        await page.locator('div.Card').wait_for()
+        #await page.locator('canvas.svelte-d1gpxk').wait_for()
+        #await page.locator('img.WeaponIcon.svelte-gp6viv').wait_for()
+        #await page.locator('canvas.ArtifactIcon').wait_for()
+        time.sleep(3)
+        await page.locator('div.Card').screenshot(path=path / f"{uid}.png")
+        await page.close()
+        #await browser.close()
+        for file in os.listdir(path):
+            if f"{uid}.png" != file:
+                file = path / file
+                try:
+                    file.unlink()
+                except:
+                    pass
+        return image(path / f"{uid}.png")
+    except Exception as e:
+        print(e)
+        await page.close()
+        #await browser.close()
