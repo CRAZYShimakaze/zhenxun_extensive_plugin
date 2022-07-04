c73de918e46f7e840330f0d05d7b6faa4a3bc5a7
CRAZYShimakaze <674015283@qq.com>
2022-07-04 23:41:28 +0800

    优化查询逻辑

--- genshin_role_card/__init__.py
+++ genshin_role_card/__init__.py
@@ -4,24 +4,26 @@ from utils.utils import get_bot, scheduler
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageEvent, Message
 from services.log import logger
-from configs.path_config import IMAGE_PATH
-from .data_source import get_alc_image
+from configs.path_config import TEMP_PATH
+from .data_source import get_alc_image, get_char_list
 from nonebot.params import CommandArg
 from utils.manager import group_manager
 from configs.config import Config
+from utils.http_utils import AsyncHttpx
+import os
 
 __zx_plugin_name__ = "原神角色卡"
 __plugin_usage__ = """
 usage：
-    查询橱窗第一个角色的面板
+    查询橱窗内角色的面板
     指令：
         原神角色卡 uid
         原神角色卡 uid 角色名
 """.strip()
-__plugin_des__ = "查询橱窗第一个角色的面板"
+__plugin_des__ = "查询橱窗内角色的面板"
 __plugin_cmd__ = ["原神角色卡 [uid] ?[角色名]"]
 __plugin_type__ = ("原神相关", )
-__plugin_version__ = 0.1
+__plugin_version__ = 0.0
 __plugin_author__ = "CRAZYSHIMAKAZE"
 __plugin_settings__ = {
     "level": 5,
@@ -29,14 +31,9 @@ __plugin_settings__ = {
     "limit_superuser": False,
     "cmd": ["原神角色卡"],
 }
-__plugin_block_limit__ = {
-    "rst": "正在查询中，请当前请求完成...",
-}
 
 char_card = on_command("原神角色卡", priority=15, block=True)
 
-CARD_PATH = IMAGE_PATH / "genshin" / "char_card"
-CARD_PATH.mkdir(parents=True, exist_ok=True)
 characters = {
     '钟离': 'zhongli',
     '神里绫华': 'ayaka',
@@ -48,12 +45,11 @@ characters = {
     '夜兰': 'yelan',
     '七七': 'qiqi',
     '优菈': 'eula',
-    '八重神子': 'yaemiko',
+    '八重神子': 'yae',
     '可莉': 'klee',
     '宵宫': 'yoimiya',
     '枫原万叶': 'kazuha',
     '珊瑚宫心海': 'kokomi',
-    '琴': 'jean',
     '申鹤': 'shenhe',
     '荒泷一斗': 'itto',
     '莫娜': 'mona',
@@ -61,39 +57,114 @@ characters = {
     '阿贝多': 'albedo',
     '魈': 'xiao',
     '神里绫人': 'ayato',
-    '刻晴': 'keqing'
+    '刻晴': 'keqing',
+    '琴': 'qinc',
+    '凝光': 'ningguang',
+    '香菱': 'xiangling',
+    '烟绯': 'feiyan',
+    '重云': 'chongyun',
+    '班尼特': 'bennett',
+    '九条裟罗': 'sara',
+    '砂糖': 'sucrose',
+    '诺艾尔': 'noelle',
+    '迪奥娜': 'diona',
+    '芭芭拉': 'barbara',
+    '早柚': 'sayu',
+    '安柏': 'amber',
+    '埃洛伊': 'aloy',
+    '云堇': 'yunjin',
+    '罗莎莉亚': 'rosaria',
+    '凯亚': 'kaeya',
+    '北斗': 'beidou',
+    '雷泽': 'razor',
+    '行秋': 'xingqiu',
+    '丽莎': 'lisa',
+    '托马': 'tohma',
+    '辛焱': 'xinyan',
+    '久岐忍': 'shinobu',
+    '五郎': 'gorou',
+    '菲谢尔': 'fischl'
 }
+char_occupy = False
 
 
 @char_card.handle()
 async def _(event: MessageEvent, arg: Message = CommandArg()):
+    global char_cache, char_occupy
+    if char_occupy:
+        await char_card.finish("当前正有角色正在查询,请稍后再试...")
+    #char_occupy = True
     msg = arg.extract_plain_text().strip().split()
     #print(msg)
     try:
         uid = int(msg[0])
     except:
         await char_card.send("请输入正确uid...")
+        char_occupy = False
         return
     if len(msg) == 2:
         if msg[1] in characters:
             chara = characters[msg[1]]
-            await char_card.send("开始获取角色信息,预计需要60秒...")
+            await char_card.send("开始获取角色信息...")
         else:
-            await char_card.send("请输入正确五星角色名...")
+            await char_card.send("请输入正确角色名...")
+            char_occupy = False
             return
     else:
         chara = 'none'
-        await char_card.send("未指定角色,默认橱窗第一位,预计需要60秒...")
-    try:
-        alc_img = await get_alc_image(CARD_PATH, str(uid), chara)
-    except Exception as e:
-        logger.info(f"{e}")
+        await char_card.send("未指定角色,默认橱窗第一位...")
+    char_list, page = await get_char_list(str(uid))
+    logger.info(f"角色获取完成！{char_list}")
+    if char_list != None:
+        char_hanzi = []
+        char_lower = [item.lower() for item in char_list]
+        for item in characters.keys():
+            for tar in char_lower:
+                if characters[item] in tar:
+                    char_hanzi.append(item)
+                    break
+    else:
+        char_occupy = False
+        await char_card.send(f"获取UID({str(uid)})角色信息超时,请检查是否已开放详细信息权限！",
+                             at_sender=True)
         return
+    alc_img = await get_alc_image(str(uid), chara, page, char_list)
+    logger.info(f"角色卡获取完成！")
     if alc_img:
-        mes = alc_img + f"\nUID({str(uid)})的角色卡片"
+        mes = alc_img + f"\n可查询角色:{','.join(char_hanzi)}"
         await char_card.send(mes, at_sender=True)
+        char_occupy = False
         logger.info(
             f"(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else 'private'})"
             f" 发送原神角色卡")
     else:
-        await char_card.send(f"获取UID({str(uid)})角色信息失败,请检查是否已开放橱窗详细信息权限或稍后再试!")
+        await char_card.send(
+            f"获取UID({str(uid)})角色信息失败,请检查是否已放入指定角色!\n可查询角色:{','.join(char_hanzi)}",
+            at_sender=True)
+        char_occupy = False
+    #try:
+    #    version = await check_update()
+    #    if float(version) > __plugin_version__ and version != '':
+    #        logger.info(
+    #            f"[genshin_role_card]发现插件版本更新{version}!请前往github项目获取更新！")
+    #except Exception as e:
+    #    logger.warning(f"{e}")
+
+
+async def check_update() -> str:
+    version_path = TEMP_PATH / '__version__'
+    url = "https://raw.githubusercontent.com/CRAZYShimakaze/zhenxun_extensive_plugin/main/genshin_role_card/__init__.py"
+    try:
+        await AsyncHttpx.download_file(url, version_path)
+    except Exception as e:
+        logger.warning(f"Error downloading {url}: {e}")
+    with version_path.open("r", encoding="utf-8") as f:
+        for item in f.readlines():
+            if str(__plugin_version__) in item:
+                new_version = item.split('=')[-1].strip()
+                break
+        else:
+            new_version = '0.0'
+    os.unlink(version_path)
+    print(new_version)
+    return new_version
--- genshin_role_card/data_source.py
+++ genshin_role_card/data_source.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from utils.message_builder import image
 from datetime import datetime
-from pathlib import Path
 from utils.http_utils import AsyncPlaywright
 from utils.http_utils import get_browser
 from nonebot.adapters.onebot.v11 import MessageSegment
@@ -10,34 +9,60 @@ import os
 import time
 import pypinyin
 from bs4 import BeautifulSoup
+from services.log import logger
+
+browser_genshin = None
 
 
-async def get_alc_image(path: Path, uid: str,
-                        chara: str) -> Optional[MessageSegment]:
+#page_genshin = None
+async def get_char_list(uid: str):
+    global browser_genshin  #, page_genshin
+    url = f"https://enka.shinshin.moe/u/{uid}"
+    s = time.time()
+    if browser_genshin == None:
+        browser_genshin = await get_browser()
+    if 1:  #page_genshin == None:
+        page_genshin = await browser_genshin.new_page()
+    try:
+        await page_genshin.goto(url, timeout=100000)
+        logger.info(f"网页打开完成！{str(time.time()-s)}s")
+        s = time.time()
+        await page_genshin.set_viewport_size({"width": 2560, "height": 1080})
+        html = await page_genshin.inner_html(".CharacterList", timeout=100000)
+        logger.info(f"角色列表打开完成！{str(time.time()-s)}s")
+        s = time.time()
+        soup = BeautifulSoup(html, "html.parser")
+        styles = [figure["style"] for figure in soup.find_all("figure")]
+        return styles, page_genshin
+    except:
+        page_genshin.close()
+        return None, None
+
+
+async def get_alc_image(uid, chara, page_genshin, styles):
     """
     截取卡片
     :param path: 存储路径
     """
-    #print(chara)
-    url = f"https://enka.shinshin.moe/u/{uid}"
-    browser = await get_browser()
-    page = await browser.new_page()
     try:
-        await page.goto(url, timeout=300000)
-        await page.set_viewport_size({"width": 2560, "height": 1080})
-        await page.evaluate(
+        s = time.time()
+        await page_genshin.evaluate(
             "document.getElementsByClassName('Dropdown-list')[0].children[13].dispatchEvent(new Event('click'));"
         )
+        logger.info("切换中文完成！")
         if chara == 'none':
-            await page.wait_for_load_state("networkidle", timeout=300000)
-            await page.locator('div.Card').screenshot(path=path / f"{uid}.png")
-            await page.close()
-            #await browser.close()
-            return image(path / f"{uid}.png")
-        html = await page.inner_html(".CharacterList", timeout=300000)
-        soup = BeautifulSoup(html, "html.parser")
-        styles = [figure["style"] for figure in soup.find_all("figure")]
-        #print(styles)
+            # Click [placeholder="Custom\ text\.\.\."]
+            await page_genshin.click("[placeholder=\"Custom\\ text\\.\\.\\.\"]"
+                                     )
+            # Fill [placeholder="Custom\ text\.\.\."]
+            await page_genshin.fill("[placeholder=\"Custom\\ text\\.\\.\\.\"]",
+                                    f"UID({uid})")
+            await page_genshin.wait_for_load_state("networkidle",
+                                                   timeout=100000)
+            card = image(await page_genshin.locator('div.Card').screenshot())
+            await page_genshin.close()
+            #await browser_genshin.close()
+            return card
         index = -1
         chara_src = ""
         for i, style in enumerate(styles):
@@ -46,26 +71,25 @@ async def get_alc_image(path: Path, uid: str,
                 chara_src = style
                 break
         if index == -1 or not chara_src:
-            return
-        await page.locator(f'div.avatar.svelte-188i0pk >> nth={index}').click()
+            return None
+        await page_genshin.locator(f'div.avatar.svelte-188i0pk >> nth={index}'
+                                   ).click()
+        logger.info(f"切换对应角色完成！{str(time.time()-s)}s")
+        s = time.time()
         # Click [placeholder="Custom\ text\.\.\."]
-        await page.click("[placeholder=\"Custom\\ text\\.\\.\\.\"]")
+        await page_genshin.click("[placeholder=\"Custom\\ text\\.\\.\\.\"]")
         # Fill [placeholder="Custom\ text\.\.\."]
-        await page.fill("[placeholder=\"Custom\\ text\\.\\.\\.\"]",
-                        f"UID({uid})")
-        await page.wait_for_load_state("networkidle", timeout=300000)
-        await page.locator('div.Card').screenshot(path=path / f"{uid}.png")
-        await page.close()
-        #await browser.close()
-        for file in os.listdir(path):
-            if f"{uid}.png" != file:
-                file = path / file
-                try:
-                    file.unlink()
-                except:
-                    pass
-        return image(path / f"{uid}.png")
+        await page_genshin.fill("[placeholder=\"Custom\\ text\\.\\.\\.\"]",
+                                f"UID({uid})")
+        await page_genshin.wait_for_load_state("networkidle", timeout=100000)
+        logger.info(f"角色载入完成！{str(time.time()-s)}s")
+        s = time.time()
+        card = image(await page_genshin.locator('div.Card').screenshot())
+        await page_genshin.close()
+        #await browser_genshin.close()
+        return card
     except Exception as e:
         print(e)
-        await page.close()
+        await page_genshin.close()
         #await browser.close()
+        return None
