d58bfe55e20c4eeb7fa4081746f29d818a9e61bc
CRAZYShimakaze <674015283@qq.com>
2023-03-24 01:04:56 +0800

    给Unknown擦屁股🐛🐛🐛

--- join_group_check/__init__.py
+++ join_group_check/__init__.py
@@ -1,8 +1,6 @@
-from nonebot import on_request
 from configs.config import Config, NICKNAME
-from nonebot.adapters.onebot.v11 import (
-    Bot
-)
+from nonebot import on_request
+from nonebot.adapters.onebot.v11 import (Bot)
 
 __zx_plugin_name__ = "入群检测 [Hidden]"
 __plugin_version__ = 0.1
@@ -25,4 +23,5 @@ async def _(bot: Bot, event):
         if event.user_id != int(bot.self_id):
             if event.request_type == 'group':
                 stranger_info = await bot.get_stranger_info(user_id=event.user_id, no_cache=False)
-                await join_group_handle.finish("[[_task|join_group]]" + "{}检测到加群请求哟~管理员们快去看看叭！\nID：{}\n昵称：{}\n性别：{}\n年龄：{}\nQQ等级：{}\n描述：{}".format(NICKNAME, event.user_id, stranger_info["nickname"], stranger_info["sex"] if stranger_info["sex"] != "unknown" else "保密", stranger_info["age"], stranger_info["level"], event.comment))
+                await join_group_handle.finish(
+                    "[[_task|join_group]]" + f'{NICKNAME}检测到加群请求哟~管理员们快去看看叭！\nID：{event.user_id}\n昵称：{stranger_info["nickname"]}\n性别：{stranger_info["sex"] if stranger_info["sex"] != "unknown" else "保密"}\n年龄：{stranger_info["age"]}\nQQ等级：{stranger_info["level"]}\n描述：{event.comment}')
