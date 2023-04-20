eded32b001f1a624fef3ec68109a2dd84dd622ec
unknownsno <hejun.1688@gmail.com>
2023-04-20 14:57:02 +0800

    添加对懒加载网页的处理

--- call/__init__.py
+++ call/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 import nonebot
 from configs.path_config import TEMP_PATH
 from nonebot import Driver
@@ -7,8 +8,10 @@ from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.params import CommandArg
 from nonebot.permission import SUPERUSER
+from bs4 import BeautifulSoup
+from utils.message_builder import image
 
-from utils.http_utils import AsyncPlaywright
+from utils.http_utils import AsyncPlaywright, AsyncHttpx
 
 driver: Driver = nonebot.get_driver()
 
@@ -21,9 +24,9 @@ usage：
 """.strip()
 __plugin_des__ = "网页截图"
 __plugin_cmd__ = ["call [url]"]
-__plugin_type__ = ("一些工具",)
-__plugin_version__ = 0.1
-__plugin_author__ = "CRAZYSHIMAKAZE"
+__plugin_type__ = ("一些工具", )
+__plugin_version__ = 0.2
+__plugin_author__ = "CRAZYSHIMAKAZE, unknownsno"
 __plugin_settings__ = {
     "level": 5,
     "default_status": True,
@@ -31,11 +34,84 @@ __plugin_settings__ = {
     "cmd": __plugin_cmd__,
 }
 
-call = on_command("call", aliases={"ck"}, permission=SUPERUSER, priority=4, block=True)
+call = on_command("call",
+                  aliases={"ck"},
+                  permission=SUPERUSER,
+                  priority=4,
+                  block=True)
 
 
 @call.handle()
 async def _(arg: Message = CommandArg()):
     url = arg.extract_plain_text().strip()
     url = url if url.startswith(('https://', 'http://')) else f'https://{url}'
-    await call.send(await AsyncPlaywright.screenshot(url, TEMP_PATH / "call.png", viewport_size=dict(width=1920, height=2048), element=[]))
+    path = TEMP_PATH / "call.png"
+    timeout = 30000
+    try:
+        async with AsyncPlaywright.new_page(
+                viewport=dict(width=1920, height=1080)) as page:
+            await page.goto(url, timeout=timeout)
+
+            # 设置竖直滚动起始点和步长
+            start_position = 0
+            scrollHeight = await page.evaluate(
+                'window.document.body.scrollHeight')
+            step = 500
+            if lazyload_test(url) or scrollHeight > 2000:
+                # 竖直滚动并等待页面加载
+                while (start_position < (scrollHeight)):
+                    start_position += step
+                    position = str(start_position)
+                    run_scroll = 'window.scrollTo(0,' + position + ')'
+                    await page.evaluate(run_scroll)
+                    await page.wait_for_timeout(1000)
+                await page.wait_for_load_state()
+
+            # 对bilibili特殊处理
+            if "bilibili" in url:
+                for element in [
+                        ".bili-feed4 .bili-header .slide-down", ".login-tip",
+                        ".header-channel-fixed"
+                ]:
+                    if await page.locator(f'{element}').is_visible():
+                        await page.evaluate(
+                            f"window.document.querySelector('{element}') \
+                            .style.display='none'")
+                        await page.wait_for_load_state()
+                if not bool(re.search(r"\d+", url)):
+                    scrollHeight = await page.evaluate(
+                        'window.document.body.scrollHeight')
+                    card = await page.screenshot(clip={
+                        "x": 0,
+                        "y": 0,
+                        "width": 1920,
+                        "height": scrollHeight - 955
+                    },
+                                          timeout=timeout,
+                                          full_page=True,
+                                          path=path)
+            else:
+                card = await page.screenshot(timeout=timeout,
+                                             full_page=True,
+                                             path=path)
+            assert card
+    except Exception as e:
+        raise e
+    await call.finish(image(path))
+
+
+async def lazyload_test(url: str):
+    '''判断懒加载'''
+    response = await AsyncHttpx.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    lazyload_exist = False
+    for tag in soup.findAll():
+        if tag.has_attr("class") and "lazyload" in tag["class"]:
+            lazyload_exist = True
+            break
+        elif tag.has_attr("data-src") or tag.has_attr("data-original"):
+            lazyload_exist = True
+            break
+
+    return lazyload_exist
