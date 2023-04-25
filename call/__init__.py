67e1f54be064b054efc0a9b9cbfeac2dac5ac982
unknownsno <hejun.1688@gmail.com>
2023-04-25 23:12:15 +0800

    添加绕过域名配置项

--- call/__init__.py
+++ call/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import traceback
 from copy import deepcopy
 import re
+
 from httpx import Response
 import chardet
 
@@ -22,6 +23,7 @@ from configs.path_config import TEMP_PATH
 from configs.config import Config
 from utils.message_builder import image
 from utils.http_utils import AsyncPlaywright, AsyncHttpx
+from utils.utils import get_local_proxy
 
 driver: Driver = nonebot.get_driver()
 
@@ -35,7 +37,7 @@ usage：
 __plugin_des__ = "网页截图"
 __plugin_cmd__ = ["call [url]"]
 __plugin_type__ = ("一些工具",)
-__plugin_version__ = 0.3
+__plugin_version__ = 0.4
 __plugin_author__ = "CRAZYSHIMAKAZE, unknownsno"
 __plugin_settings__ = {
     "level": 5,
@@ -51,6 +53,14 @@ Config.add_plugin_config(
     help_="是否忽略懒加载，默认True（改为False后即可截长图，单个图片8M左右，酌情选择）",
     default_value=True,
 )
+Config.add_plugin_config(
+    "call",
+    "PROXY_BYPASS",
+    ["raw.kgithub.com"],
+    name="call",
+    help_="如遇到代理规则无法绕过的域名，导致截图失败，可将该域名填到下方，注意格式一致",
+    default_value=["raw.kgithub.com"],
+)
 
 call = on_command("call", aliases={"ck"}, permission=SUPERUSER, priority=4, block=True)
 
@@ -60,15 +70,22 @@ async def _(event: Event, arg: Message = CommandArg()):
     url = arg.extract_plain_text().strip()
     url = url if url.startswith(('https://', 'http://')) else f'https://{url}'
     group_id = event.group_id if isinstance(event, GroupMessageEvent) else None
+    bypass = tuple(x if x.startswith(('https://', 'http://')) else f'https://{x}' for x in
+                   Config.get_config("call", "PROXY_BYPASS"))
+    use_proxy = not url.startswith(bypass)
+    ignore_lazyload = Config.get_config("call", "IGNORE_LAZYLOAD")
     try:
-        response = deepcopy(await AsyncHttpx.get(url, follow_redirects=True))
+        response = deepcopy(await AsyncHttpx.get(url, follow_redirects=True, use_proxy=use_proxy))
         url = str(response.url)
+        logger.info(f"REDIRECTS AT: {url} USE_PROXY: {use_proxy} IGNORE_LAZYLOAD: {ignore_lazyload}", "call",
+                    user_id=event.get_user_id(),
+                    group_id=group_id)
     except Exception as e:
-        logger.error("截图失败", "call", user_id=event.get_user_id(), group_id=group_id, e=e)
+        logger.error(f"截图失败\n{traceback.format_exc()}", "call", user_id=event.get_user_id(), group_id=group_id, e=e)
         return await call.send("截图失败")
     path = TEMP_PATH / "call.png"
-    timeout = 30000
-    if Config.get_config("call", "IGNORE_LAZYLOAD"):
+    timeout = 20000
+    if ignore_lazyload:
         try:
             card = await AsyncPlaywright.screenshot(url,
                                                     path,
@@ -85,7 +102,7 @@ async def _(event: Event, arg: Message = CommandArg()):
             return await call.send(image(await text_to_pic(text=response.text,
                                                            width=540)))
         except Exception as e:
-            logger.error("截图失败",
+            logger.error(f"截图失败\n{traceback.format_exc()}",
                          "call",
                          user_id=event.get_user_id(),
                          group_id=group_id,
@@ -103,7 +120,8 @@ async def _(event: Event, arg: Message = CommandArg()):
             start_position = 0
             scroll_height = await page.evaluate('window.document.body.scrollHeight')
             step = 500
-            if await lazyload_test(url, response) or scroll_height > 2000:
+            if await lazyload_test(url, response, use_proxy) or scroll_height > 2000:
+                logger.info(f"开始滚动页面", "call", user_id=event.get_user_id(), group_id=group_id)
                 # 竖直滚动并等待页面加载
                 while start_position < scroll_height:
                     start_position += step
@@ -115,6 +133,7 @@ async def _(event: Event, arg: Message = CommandArg()):
 
             # 对bilibili特殊处理
             if "bilibili" in url:
+                logger.info(f"特殊处理bilibili", "call", user_id=event.get_user_id(), group_id=group_id)
                 for element in [
                     ".bili-feed4 .bili-header .slide-down", ".login-tip",
                     ".header-channel-fixed"
@@ -137,21 +156,23 @@ async def _(event: Event, arg: Message = CommandArg()):
                         path=path)
                 else:
                     card = await page.screenshot(timeout=timeout, full_page=True, path=path)
+            else:
+                card = await page.screenshot(timeout=timeout, full_page=True, path=path)
             assert card
     except Error:
         if not chardet.detect(response.content).get("encoding"):
             return await call.send("检测到乱码，取消截图")
         return await call.send(image(await text_to_pic(text=response.text, width=540)))
     except Exception as e:
-        logger.error("截图失败", "call", user_id=event.get_user_id(), group_id=group_id, e=e)
-        return await call.send("截图失败")
+        logger.error(f"长截图失败\n{traceback.format_exc()}", "call", user_id=event.get_user_id(), group_id=group_id, e=e)
+        return await call.send("长截图失败")
     await call.finish(image(path))
 
 
-async def lazyload_test(url: str, response: Response):
+async def lazyload_test(url: str, response: Response, use_proxy: bool):
     """判断懒加载"""
     if response.status_code != 200:
-        response = await AsyncHttpx.get(url, follow_redirects=True)
+        response = await AsyncHttpx.get(url, follow_redirects=True, use_proxy=use_proxy)
     soup = BeautifulSoup(response.content, "html.parser")
 
     lazyload_exist = False
