16fabb4d747f5f3b5a89f8555fb5ff185bee23e7
CRAZYShimakaze <674015283@qq.com>
2024-11-10 16:19:58 +0800

    fix

--- plugin_utils/auth_utils.py
+++ plugin_utils/auth_utils.py
@@ -2,7 +2,6 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message
 from nonebot.exception import FinishedException
 
 from models.bag_user import BagUser
-from utils.utils import get_bot
 
 
 async def check_gold(event, coin: int, percent: int = 0):
@@ -11,7 +10,7 @@ async def check_gold(event, coin: int, percent: int = 0):
         if percent:
             coin = coin if user_coin * percent // 100 < coin else user_coin * percent // 100
         if user_coin < coin:
-            bot = get_bot()
+            bot = nonebot.get_bot()
             await bot.send_group_msg(group_id=event.group_id, message=Message(
                 f"该功能需要{coin}金币,你的金币不够！(请发送'签到'获取金币.)"))
             raise FinishedException
